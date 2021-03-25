7737c9184e8fbc0426d65c9a5f543fe89b100079
Alluisio Silva <alluisio.silva@ipdec.org>
2021-03-24 23:56:30 -0400

    Criação da tela básica

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
-import { SectionText, SectionView } from './styles';
+import { SectionImage, SectionView } from './styles';
+import SplashLogo from '../../assets/img/SevenHousesLogo.png';
+import { StatusBar } from 'react-native';
 
 export const SplashScreen = () => {
   return (
     <SectionView>
-      <SectionText>Bem Vindo ao Find Houses!!!</SectionText>
+      <StatusBar translucent backgroundColor="transparent" />
+      <SectionImage source={SplashLogo} resizeMode="contain" />
     </SectionView>
   );
 };
