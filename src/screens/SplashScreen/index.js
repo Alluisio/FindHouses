80fd204b5bdafeaec1d7eaf74c41d68e1198f8b5
Alluisio Silva <alluisio.silva@ipdec.org>
2021-03-24 21:13:16 -0400

    Substituição do StyleSheet por styled-component
    1. Foi criado o arquivo styles para comportar os componentes personalizados da tela SplashScreen
    2. Foi modificado o componente de View para SectionView (componente personalizado);
    3. Foi modificado o componente para exibição de texto para SectionText (componente personalizado);

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SectionText, SectionView } from './styles';
 
 export const SplashScreen = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.sectionView}>
-        <Text style={styles.sectionText}>Bem Vindo ao Find Houses!!!</Text>
-      </View>
-    </SafeAreaView>
+    <SectionView>
+      <SectionText>Bem Vindo ao Find Houses!!!</SectionText>
+    </SectionView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionView: {
-    height: 100,
-    width: '100%',
-    backgroundColor: 'red',
-  },
-  sectionText: {
-    color: 'white',
-  },
-});
---
+++ src/screens/SplashScreen/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const SectionView = styled.View`
+  background-color: red;
+  width: 100%;
+  height: 100px;
+`;
+
+export const SectionText = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+`;
