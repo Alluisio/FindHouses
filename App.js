54da8795eb0d9e715c73bfe63d2441e9c731a00b
Alluisio Silva <alluisio.silva@ipdec.org>
2021-03-24 20:37:51 -0400

    Modificação da View
    1. Foi modificado o tamanho e a cor  da View

--- App.js
+++ App.js
@@ -5,13 +5,19 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <SafeAreaView>
-      <View>
+      <View style={styles.sectionView}>
         <Text>Bem Vindo ao Find Houses!!!</Text>
       </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  sectionView: {
+    height: 100,
+    width: '100%',
+    backgroundColor: 'red',
+  },
+});
 
 export default App;
