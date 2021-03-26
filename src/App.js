17fb8d973006d15cc8176aa428eceaad6d576841
Alluisio Silva <alluisio.silva@ipdec.org>
2021-03-26 00:11:58 -0400

    Implementação do story book

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { SplashScreen } from './screens';
 import { theme } from './styles/theme';
+import StoreBook from '../storybook';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <SplashScreen />
+      <StoreBook />
     </ThemeProvider>
   );
 };
