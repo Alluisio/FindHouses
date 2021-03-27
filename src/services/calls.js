926b3640aef1435e62a9838467eb997074ffde79
Alluisio Silva <alluisio.silva@ipdec.org>
2021-03-26 20:58:29 -0400

    Implementação com a api

---
+++ src/services/calls.js
@@ -0,0 +1,13 @@
+import api from './api';
+
+export const getHousesCall = async () => {
+  try {
+    const result = await api.get(
+      'properties/v2/list-for-rent?city=Miami&state_code=FL&limit=15&offset=0&sort=relevance',
+    );
+    return result.data;
+  } catch (err) {
+    console.log({ err });
+    return err;
+  }
+};
